d1c4ec1b5abf60b7024d076a46ff92d77058b61f
unknown <annejain16@gmail.com>
2024-03-07 11:44:41 +0530

    first commit

---
+++ dashboard-app/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import HeaderDashboard from './HeaderDashboard';
+
+function App() {
+  return (
+    <div className="App">
+    <HeaderDashboard/> 
+    </div>
+  );
+}
+
+export default App;
---
+++ dashboard-app/src/HeaderDashboard.js
@@ -0,0 +1,10 @@
+import logo from './logo.svg';
+import './App.css';
+
+function HeaderDashboard() {
+  return (
+   <p>Hello World!</p>
+  );
+}
+
+export default HeaderDashboard;
